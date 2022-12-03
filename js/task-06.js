52c5dcb194fb3a2d6fff9295b65e40829668ae9f
MaksymTykhoniuk <mxtkhnk@gmail.com>
2022-12-03 19:50:53 +0200

    fix

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,10 @@ const input = document.querySelector("#validation-input");
 input.addEventListener(`input`, onInputBlur);
 function onInputBlur(event) {
   if (event.currentTarget.value.length <= 6) {
-    input.classList.add(`valid`);
-    input.classList.remove(`invalid`);
-  } else {
     input.classList.remove(`valid`);
     input.classList.add(`invalid`);
+  } else {
+    input.classList.add(`valid`);
+    input.classList.remove(`invalid`);
   }
 }
